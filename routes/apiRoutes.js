01b69640da749acc69a0ae84c0b26dc9ac6469a3
Mamessile Kiman <sile.kiman@yahoo.com>
2020-01-12 23:03:39 -0500

    Updated the delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,11 @@
-// ===============================================================================
-// LOAD DATA
-// ===============================================================================
+//Set up all the required modules/data and dependencies 
 
 var noteData = require("../data/noteData.json");
 const fs = require('fs');
 
- 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
+ 
+//Setup API data Routing
 module.exports = function (app) {
 
   //Api page route
@@ -17,53 +13,61 @@ module.exports = function (app) {
     var data = fs.readFileSync('data/noteData.json')
     var notes = JSON.parse(data);
     return res.json(notes);
-      
-  });
-   
-// A function for saving a note to the db
-app.post("/api/notes", (req, res) => {
-  var id= 0;
-  const newNote = req.body ;
-  for (var i=1; i<noteData.length; i++){
-    id ++;
-     
-    if(newNote){
-       newNote.id = id;  
-      }
-      else{
-      return
-      }
-     
-}
-  noteData.push(newNote);
-  console.log("new pushed:", noteData)
-  var notes = JSON.stringify(noteData);
-  var data = fs.writeFileSync('data/noteData.json', notes)
-  console.log("from post:", notes)
-   
-  return res.json(data);
-  
-  });
- 
 
-  app.delete("/api/notes:id", (req, res) => {
-     
-      const id = req.params.id;
-      for (var i=1; i<noteData.length; i++){
-        //id ++;
-         
-        if(id=== noteData.id){
-           noteData[i]={};
-          }
-          else{
-          return
-          }
-          var notes = JSON.stringify(noteData);
-          var data = fs.writeFileSync('data/noteData.json', notes)
-          console.log("from post:", notes)
-           
-          return res.json(data);
-           
-    }
   });
+
+  // A function for saving a note to the db
+  app.post("/api/notes", (req, res) => {
+    var id = 0;
+    const newNote = req.body;
+    for (var i = 1; i < noteData.length; i++) {
+      id++;
+
+      if (newNote) {
+        newNote.id = id;
+      }
+      else {
+        return
+      }
+
+    }
+    noteData.push(newNote);
+    // var notes = JSON.stringify(noteData);
+    // var data = fs.writeFileSync('data/noteData.json', notes)
+ 
+    // return res.json(data);
+    refreshData(res);
+
+  });
+
+  //Function to delete an item from the database and reload the array
+  app.delete("/api/notes:id", (req, res) => {
+    const noteid = req.params.id;
+    for (var i = 0; i < noteData.length; i++) {
+
+      if (noteid == noteData[i].id) {
+        const index = noteData.indexOf(noteData[i]);
+        noteData.splice(index, 1);
+      }
+
+    }
+    refreshData(res);
+    // var notes = JSON.stringify(noteData);
+    // var data = fs.writeFileSync('data/noteData.json', notes)
+ 
+    // return res.json(data);
+
+  });
+}
+
+//Function to refresh the database after new entry or deletion
+var refreshData = ()=>{
+   try{
+    var notes = JSON.stringify(noteData);
+    return fs.writeFileSync('data/noteData.json', notes)
+  }
+  catch (err){
+    return [];
+  }
+
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,8 +9,10 @@ module.exports = function(app) {
   app.get("/notes", (req, res)=>{
     res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
-    // If no matching route is found default to home
-    app.get("*", (req, res)=>{
+
+
+  // If no matching route is found default to home
+  app.get("*", (req, res)=>{
         res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+  });
 };
